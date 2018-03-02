4c4925dc15c58cd8074887d94840dec3f492f823
Alexeev Yaroslav <ice-t95@mail.ru>
2018-03-02 17:41:09 +0300

    Add methods in user service and their realization

--- src/main/java/com/users/service/UserService.java
+++ src/main/java/com/users/service/UserService.java
@@ -5,5 +5,14 @@ import com.users.entity.User;
 import java.util.List;
 
 public interface UserService {
+
+    void save(User user);
+
+    void delete(int id);
+
+    User getById(int id);
+
+    void update(User user);
+
     List<User> geAllUsers();
 }
--- src/main/java/com/users/service/UserServiceImpl.java
+++ src/main/java/com/users/service/UserServiceImpl.java
@@ -10,6 +10,22 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserDao userDao;
 
+    public void save(User user) {
+        userDao.save(user);
+    }
+
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+    public User getById(int id) {
+        return userDao.getById(id);
+    }
+
+    public void update(User user) {
+        userDao.update(user);
+    }
+
     public List<User> geAllUsers() {
         //Here business logic!!!
         return userDao.geAllUsers();
